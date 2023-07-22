866551619c76e915bcf1adbfbce1246267922d17
Vevilion <0Endersoul0@gmail.com>
2023-07-23 02:08:25 +1000

    fixed browser not closing

--- tafe_timetable.py
+++ tafe_timetable.py
@@ -29,3 +29,5 @@ def timetable_lookup():
     S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
     driver.set_window_size(S('Width'),S('Height')) # May need manual adjustment                                                                                                                
     driver.find_element("xpath","//div[@id='GroupTable']").screenshot('timetable.png')
+
+    driver.quit()
